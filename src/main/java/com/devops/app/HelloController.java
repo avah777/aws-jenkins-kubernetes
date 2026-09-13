a88218cb475c8c9bddbbf9111ab7bc790a64149e
avah777 <infoservices244@gmail.com>
2026-09-13 01:15:48 +0100

    test application added

---
+++ src/main/java/com/devops/app/HelloController.java
@@ -0,0 +1,18 @@
+package com.devops.app;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String home() {
+        return "Hello from Jenkins + Docker + Kubernetes + AWS!";
+    }
+    @GetMapping("/health")
+    public Map<String, String> health() {
+        return Map.of(
+                "status", "UP",
+                "application", "aws-jenkins-kubernetes"
+        );
+    }
+}
